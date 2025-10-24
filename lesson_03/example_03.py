f327d527091bee122fd7ff2af746a547c258fc7e
PotegaIT <github@potegait.com>
2025-10-24 15:53:39 +0200

    Create example_03.py

---
+++ lesson_03/example_03.py
@@ -0,0 +1,15 @@
+# example.py
+# Prosty przykład w Pythonie dla początkujących
+# Simple Python example for beginners
+
+# Wyświetlenie powitania
+# Displaying a greeting
+print("Hello, World!")
+
+# Kilka osobistych komunikatów
+print("Cześć, programowanie jest super!")
+print("To mój pierwszy program w Pythonie.")
+
+# A few personal messages
+print("Hi, programming is awesome!")
+print("This is my first Python program.")
