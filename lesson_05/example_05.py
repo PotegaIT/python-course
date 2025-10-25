d245af7c9d36f731bea282e3f85b7179d6c6d46f
PotegaIT <github@potegait.com>
2025-10-25 09:15:56 +0200

    Create example_05.py

---
+++ lesson_05/example_05.py
@@ -0,0 +1,10 @@
+city = "Oslo"        # zmienna typu string (tekst) / variable of type string (text)
+year = 2025          # zmienna typu integer (liczba całkowita) / variable of type integer (whole number)
+temperature = 4.5    # zmienna typu float (liczba zmiennoprzecinkowa) / variable of type float (floating-point number)
+is_raining = False   # zmienna typu bool (wartość logiczna: True lub False) / variable of type bool (logical value: True or False)
+
+# Wyświetlamy typy danych każdej zmiennej / Display the data type of each variable
+print(type(city))        # <class 'str'>
+print(type(year))        # <class 'int'>
+print(type(temperature)) # <class 'float'>
+print(type(is_raining))  # <class 'bool'>
