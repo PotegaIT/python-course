9fceee046aecabf9bf29e96e751df47a95af5955
PotegaIT <github@potegait.com>
2025-11-07 15:12:38 +0100

    Create main.py

---
+++ lesson_23/main.py
@@ -0,0 +1,3 @@
+from moj_modul import powiedz_czesc  # Importuje funkcję powiedz_czesc z modułu moj_modul / Imports the function powiedz_czesc from the module moj_modul
+
+powiedz_czesc("Julia")  # Wywołuje funkcję, która wypisuje powitanie / Calls the function to print a greeting
