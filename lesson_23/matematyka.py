836fb46833bce5601d9d8c4970556393a15ab975
PotegaIT <github@potegait.com>
2025-11-07 15:10:25 +0100

    Create matematyka.py

---
+++ lesson_23/matematyka.py
@@ -0,0 +1,8 @@
+def kwadrat(x): 
+    return x ** 2  # Zwraca kwadrat liczby / Returns the square of the number
+
+def srednia(lista): 
+    return sum(lista) / len(lista)  # Zwraca średnią z listy liczb / Returns the average of a list of numbers
+
+def czy_parzysta(x): 
+    return x % 2 == 0  # Zwraca True, jeśli liczba jest parzysta / Returns True if the number is even
