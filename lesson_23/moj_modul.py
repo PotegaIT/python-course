5f0676b7e8e898a98c14cfdcff1dcdbc600a0c2a
PotegaIT <github@potegait.com>
2025-11-07 15:11:30 +0100

    Create moj_modul.py

---
+++ lesson_23/moj_modul.py
@@ -0,0 +1,5 @@
+def powiedz_czesc(imie): 
+    print(f"Cześć, {imie}!")  # Wypisuje powitanie dla podanego imienia / Prints a greeting for the given name
+
+def dodaj(a, b): 
+    return a + b  # Zwraca sumę dwóch liczb / Returns the sum of two numbers
